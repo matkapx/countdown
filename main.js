da81e36ccdadce906088444d80291bbf44635e19
Fatih Alpay <matkapx@gmail.com>
2022-12-27 16:02:05 +0300

    init2

---
+++ main.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Unix timestamp (in seconds) to count down to
+    var opts = {headings: ["Tage", "Stunde", "Minutes", "Sekunden"]};
+    var twoDaysFromNow = (new Date("Oct 1, 2023 10:00:00").getTime() / 1000) + (86400 * 2) + 1;
+  
+    // Set up FlipDown
+    var flipdown = new FlipDown(twoDaysFromNow, opts)
+  
+      // Start the countdown
+      .start()
+  
+      // Do something when the countdown ends
+      .ifEnded(() => {
+        console.log('The countdown has ended!');
+      });
+  
+  });
